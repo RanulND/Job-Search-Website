6e97e5625ed7232f476fe8f843ca6c0d0632124a
RanulND <ranulnavojith.99@gmail.com>
2022-11-03 00:15:28 +0530

    first commit

---
+++ src/containers/LoginContainer/LoginContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./LoginContainer.css"
+const Login = () => {
+
+    return(
+        <div className="login container">
+             <div className="login-1">
+                <div className="">Hehe</div>
+             </div>
+        </div>
+    )
+}
+
+export default Login;
