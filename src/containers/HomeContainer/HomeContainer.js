1f5b4a456819dac39321f818aff9f5e5e290d076
RanulND <ranulnavojith.99@gmail.com>
2022-11-07 23:33:22 +0530

    added Navbar

--- src/containers/HomeContainer/HomeContainer.js
+++ src/containers/HomeContainer/HomeContainer.js
@@ -1,13 +1,18 @@
 import React from "react";
 import JobCard from "../../components/JobCardComponent/JobCardComponent";
+import Navbar from "../../components/NavbarComponent/NavbarComponent";
 
 const Home = () => {
 
     return(
-        <div className="container home py-4 px-5">
-            <JobCard />
-            <JobCard />
+        <div>
+            <Navbar />
+            <div className="container home py-4 px-5">
+                <JobCard />
+                <JobCard />
+            </div>
         </div>
+        
     )
 }
 
